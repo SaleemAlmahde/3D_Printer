ae7bd9e9c13cf93c3a6cef5fcc6c4e6cef8c838d
SaleemAlmahde <almahdesaleem@gmail.com>
2026-01-18 00:24:18 +0100

    e

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = "invoice-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
   "/index.html",
   "/invoices.html",
   "/cart.html",
@@ -27,18 +26,9 @@ const FILES_TO_CACHE = [
   "/assets/java/html2pdf.bundle.min.js",
   "/assets/java/invoice_logo.js",
   "/assets/java/invoice_print.js",
-  "/assets/java/invoice_print.js",
-  "/assets/java/invoiceCodec.js.js",
-  "/assets/java/invoice_print.js",
+  "/assets/java/invoiceCodec.js",
   "/assets/java/show_toast.js",
   "/assets/plugins/fuse/fuse.js",
-  "/assets/plugins/fonts/alfont_com_AlFont_com_K24KurdishBold-Bold-1.ttf",
-  "/assets/plugins/fonts/alfont_com_LANTX-Regular.otf",
-  "/assets/plugins/fonts/AM-ZAIN-ORIGINAL.ttf",
-  "/assets/plugins/fonts/Bahij_TheSansArabic-Bold.ttf",
-  "/assets/plugins/fonts/TheYearofTheCamel-ExtraBold.otf",
-  "/assets/plugins/fonts/uthmanic_hafs_v22.ttf",
-  "/assets/plugins/fonts/Ya-ModernPro-Bold.otf",
   "/assets/plugins/font-awesome/css/font-awesome.css",
   "/assets/plugins/font-awesome/css/font-awesome.min.css",
   "/assets/plugins/font-awesome/fonts/fontawesome-webfont.eot",
@@ -47,18 +37,20 @@ const FILES_TO_CACHE = [
   "/assets/plugins/font-awesome/fonts/fontawesome-webfont.woff",
   "/assets/plugins/font-awesome/fonts/fontawesome-webfont.woff2",
   "/assets/plugins/font-awesome/fonts/FontAwesome.otf",
-  
-  
-//   "/css/style.css",
-//   "/js/main.js"
-  
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+      // محاولة cache كل ملف على حدة لتجنب فشل العملية بأكملها
+      return Promise.allSettled(
+        FILES_TO_CACHE.map((file) =>
+          cache.add(file).catch((err) => {
+            console.warn(`فشل تخزين الملف مؤقتاً: ${file}`, err);
+          }),
+        ),
+      );
+    }),
   );
 });
 
@@ -66,6 +58,6 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
